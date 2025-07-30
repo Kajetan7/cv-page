847606524443c0eba38e70842b651c15aef2d7ef
Kajetan Mroske <kajtek@Kajetans-MacBook-Air.local>
2025-07-30 16:36:33 +0200

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.11-slim
+
+WORKDIR /app
+
+COPY . .
+
+RUN pip install --no-cache-dir mkdocs
+
+EXPOSE 8000
+
+CMD ["mkdocs", "serve", "-a", "0.0.0.0:8000"]
---
+++ scripts/docker.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+IMAGE_NAME="mkdocs-image"
+CONTAINER_NAME="mkdocs-container"
+
+case "$1" in
+  build)
+    echo "🔨 Building docker image.."
+    docker build -t $IMAGE_NAME ../
+    ;;
+
+  start)
+    echo "🚀 Running container with port 8000.."
+    docker run -d --rm -p 8000:8000 --name $CONTAINER_NAME $IMAGE_NAME
+    ;;
+
+  stop)
+    echo "🛑 Stopping container and removing test image."
+    docker stop $CONTAINER_NAME 2>/dev/null
+    docker rmi -f $IMAGE_NAME
+    ;;
+
+  *)
+    echo "Usage: bash $0 {build|start|stop}"
+    exit 1
+    ;;
+esac
